ba7be6392d327fb300bc92c64a821186fb6c577a
Taewon Hwang <htw1203@gmail.com>
2024-01-18 20:48:28 +0900

    initial commit

---
+++ src/main/java/com/bitcafe/BitcafeApplication.java
@@ -0,0 +1,13 @@
+package com.bitcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcafeApplication.class, args);
+    }
+
+}
