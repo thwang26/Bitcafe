28c69e1c415bd30af477a50c2ad1fb73d2615837
Taewon Hwang <htw1203@gmail.com>
2024-01-30 23:17:38 +0900

    feat: profile 설정

--- src/main/java/com/bitcafe/BitcafeApplication.java
+++ src/main/java/com/bitcafe/BitcafeApplication.java
@@ -2,12 +2,23 @@ package com.bitcafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BitcafeApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(BitcafeApplication.class, args);
+        new SpringApplicationBuilder(BitcafeApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(BitcafeApplication.class, args);
+//    }
 
 }
--- src/main/java/com/bitcafe/controller/FirstController.java
+++ src/main/java/com/bitcafe/controller/FirstController.java
@@ -1,15 +1,28 @@
 package com.bitcafe.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
+@AllArgsConstructor
 public class FirstController {
+    private Environment env;
 
     @GetMapping("/hello-world")
     public ResponseEntity<String> helloWorld() {
         return new ResponseEntity<>("Hello World!2", HttpStatus.OK);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
