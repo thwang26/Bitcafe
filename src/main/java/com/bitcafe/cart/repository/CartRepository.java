55522a4ee6c6808b976391b1fe1690d662b4866d
Taewon Hwang <htw1203@gmail.com>
2024-05-06 11:50:25 +0900

    fix: fetch join 문구 수정

--- src/main/java/com/bitcafe/cart/repository/CartRepository.java
+++ src/main/java/com/bitcafe/cart/repository/CartRepository.java
@@ -4,6 +4,7 @@ package com.bitcafe.cart.repository;
 import com.bitcafe.cart.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,6 @@ import java.util.List;
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    @Query("select c from Cart c join fetch c.menu")
-    List<Cart> findAllByMemberEmail(String email);
+    @Query("select c from Cart c join fetch c.menu where c.member.id = :id")
+    List<Cart> findAllByMemberId(@Param("id") Long id);
 }
